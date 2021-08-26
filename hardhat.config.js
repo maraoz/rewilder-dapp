4dfb8ed91798a9dca6bb7dc6b977f830eb2974aa
Gabe Ragland <gabe.ragland@gmail.com>
2021-08-25 18:27:00 -0700

    Revert hardhat directory

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,6 @@
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-ethers");
-require('@openzeppelin/hardhat-upgrades');
+require("@openzeppelin/hardhat-upgrades");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -21,7 +21,7 @@ task("accounts", "Prints the list of accounts", async (_args, hre) => {
 module.exports = {
   solidity: "0.8.4",
   paths: {
-    artifacts: "./../src/artifacts",
+    artifacts: "./src/artifacts",
   },
   networks: {
     hardhat: {
