814a6b02eb70de6230a3311a5651178872f84563
Manuel Araoz <287189+maraoz@users.noreply.github.com>
2021-09-14 00:49:59 -0400

    add useBalanceOf

---
+++ src/lib/rewilderNFT.js
@@ -0,0 +1,22 @@
+import { ethers } from "ethers";
+import { useContractCall, useContractFunction } from "@usedapp/core";
+import RewilderNFT from "../artifacts/contracts/RewilderNFT.sol/RewilderNFT.json";
+import { addressFor } from "./addresses";
+const RewilderNFTInterface = new ethers.utils.Interface(RewilderNFT.abi)
+const nftAddress = addressFor("RewilderNFT");
+const nft = new ethers.Contract(
+  nftAddress,
+  RewilderNFTInterface,
+);
+
+export function useBalanceOf(address) {
+  const [balance] = useContractCall(
+    address && {
+      abi: RewilderNFTInterface,
+      address: nftAddress,
+      method: "balanceOf",
+      args: [address],
+    }
+  ) ?? [];
+  return balance;
+}
