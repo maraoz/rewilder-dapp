17a8ee7d70a06b5ca1e6f2bb86386d2602f647d6
Manuel Araoz <287189+maraoz@users.noreply.github.com>
2021-09-14 21:25:32 -0400

    Make RewilderNFT an ERC721EnumerableUpgradeable

--- contracts/RewilderNFT.sol
+++ contracts/RewilderNFT.sol
@@ -2,12 +2,13 @@
 pragma solidity ^0.8.2;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721EnumerableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol";
 
-contract RewilderNFT is Initializable, ERC721Upgradeable, OwnableUpgradeable, UUPSUpgradeable {
+contract RewilderNFT is Initializable, ERC721Upgradeable, ERC721EnumerableUpgradeable, OwnableUpgradeable, UUPSUpgradeable {
     using CountersUpgradeable for CountersUpgradeable.Counter;
 
     CountersUpgradeable.Counter private _tokenIdCounter;
@@ -17,6 +18,7 @@ contract RewilderNFT is Initializable, ERC721Upgradeable, OwnableUpgradeable, UU
 
     function initialize() initializer public {
         __ERC721_init("RewilderNFT", "WILD");
+        __ERC721Enumerable_init();
         __Ownable_init();
         __UUPSUpgradeable_init();
 
@@ -39,4 +41,22 @@ contract RewilderNFT is Initializable, ERC721Upgradeable, OwnableUpgradeable, UU
         onlyOwner
         override
     {}
+    
+    // The following functions are overrides required by Solidity.
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        override(ERC721Upgradeable, ERC721EnumerableUpgradeable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721Upgradeable, ERC721EnumerableUpgradeable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
 }
--- contracts/mocks/MockRewilderNFTv2.sol
+++ contracts/mocks/MockRewilderNFTv2.sol
@@ -2,22 +2,32 @@
 pragma solidity ^0.8.2;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721EnumerableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol";
 
-import "hardhat/console.sol";
-
-contract MockRewilderNFTv2 is
-    Initializable,
-    ERC721Upgradeable,
-    OwnableUpgradeable,
-    UUPSUpgradeable
-{
+contract MockRewilderNFTv2 is Initializable, ERC721Upgradeable, ERC721EnumerableUpgradeable, OwnableUpgradeable, UUPSUpgradeable {
     using CountersUpgradeable for CountersUpgradeable.Counter;
 
     CountersUpgradeable.Counter private _tokenIdCounter;
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() initializer {}
+
+    function initialize() initializer public {
+        __ERC721_init("RewilderNFT", "WILD");
+        __ERC721Enumerable_init();
+        __Ownable_init();
+        __UUPSUpgradeable_init();
+
+        _tokenIdCounter.increment();
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "https://app.rewilder.xyz/api/v1/";
+    }
 
     /*
      * onlyOwner modifier removed to safeMint function
@@ -29,8 +39,26 @@ contract MockRewilderNFTv2 is
 
     function _authorizeUpgrade(address newImplementation)
         internal
-        override
         onlyOwner
+        override
     {}
+    
+    // The following functions are overrides required by Solidity.
 
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        override(ERC721Upgradeable, ERC721EnumerableUpgradeable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721Upgradeable, ERC721EnumerableUpgradeable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
 }
+
