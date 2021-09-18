a27ba658c6f916a00409fb8701d2957ab97a5c3d
Agostina <agostiina.blanco@gmail.com>
2021-09-17 21:57:25 -0400

    update link variables in DonationUpdate

--- src/components/DonationUpdate.js
+++ src/components/DonationUpdate.js
@@ -1,5 +1,5 @@
 
-function DonationUpdate({ icon, iconalt, date, message, link }) {
+function DonationUpdate({ icon, iconalt, date, message, linkText, linkHref }) {
   return <div className="notification-card">
     <div className="tag d-flex justify-content-start">
       <div className="icon-avatar mr-2">
@@ -11,9 +11,9 @@ function DonationUpdate({ icon, iconalt, date, message, link }) {
       </div>
       <div className="content">
         <h5 className="fs-12 font-bold text">{date}</h5>
-        <h3 className="fs-14 font-book">{message}<i className="fas fa-external-link-alt fs-9 color-light"></i></h3>
-        <a href="#">
-          <h3 class="fs-14 font-book color-green text-decoration-underline">{link}</h3>
+        <h3 className="fs-14 font-book">{message}</h3>
+        <a href={linkHref} target="_blank">
+          <h3 className="fs-14 font-book color-green text-decoration-underline">{linkText}</h3>
         </a>
       </div>
     </div>
