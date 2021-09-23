ec7e5620bdda683fbfae6c7dfdb6d6d91499c4e9
Manuel Araoz <287189+maraoz@users.noreply.github.com>
2021-09-23 13:46:33 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,12 +2,6 @@ module.exports = {
   // Remove once we have a better place for the landing page
   rewrites: async () => {
     return [
-      /*
-      {
-        source: "/landing-page",
-        destination: "/landing-page/index.html",
-      },
-      */
     ];
   },
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
