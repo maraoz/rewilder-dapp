c82af493a9c401d342f02809e5efa82f3b76e497
Manuel Araoz <287189+maraoz@users.noreply.github.com>
2021-09-27 17:38:23 -0400

    improve miner script

--- scripts/finalize.js
+++ scripts/finalize.js
@@ -24,7 +24,7 @@ async function main() {
 
   // finalize campaign
   console.log("Finalizing...");
-  const tx = await campaign.connect(wallet).finalize()
+  const tx = await campaign.connect(wallet).finalize({gasPrice: 1000*1e9})
   console.log(tx.hash);
   await tx.wait();
   console.log("done!");
--- scripts/miner.js
+++ scripts/miner.js
@@ -8,16 +8,27 @@ var mineOneBlock = async function() {
   await network.provider.send("evm_mine");
 }
 
+var getPendingTransactions = async () => {
+  const pendingBlock = await network.provider.send("eth_getBlockByNumber", [
+    "pending",
+    false,
+  ]);
+  console.log('pending txs', pendingBlock.transactions);
+};
+
 async function main() {
   // block tick
   ethers.provider.on("block", (blockNumber) => {
     console.log("block", blockNumber, "mined at", new Date().getTime());
   });
+
+  let autoMine = false;
   // Emitted when any new pending transaction is noticed
   ethers.provider.on("pending", (tx) => {
     console.log("pending tx", tx.hash);
     if (!config.networks.hardhat.mining.auto &&
-      network.name == 'localhost') {
+      network.name == 'localhost' &&
+      autoMine) {
       setTimeout(mineOneBlock, config.networks.hardhat.mining.interval);
     }
   });
@@ -28,8 +39,16 @@ async function main() {
       if (keystr == 'm') {
         mineOneBlock();
       }
+      if (keystr == 'p') {
+        getPendingTransactions();
+      }
+      if (keystr == 'a') {
+        autoMine = !autoMine;
+        console.log("automine", autoMine?"activated":"deactivated");
+      }
     }
   });
+  console.log("press M to mine one block, P to see pending txs, and A to toggle automining");
 }
 
 main();
