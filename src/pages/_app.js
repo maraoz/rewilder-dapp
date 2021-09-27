8a708de0a5c89b59cf119aa3d2615567ef84f456
Manuel Araoz <287189+maraoz@users.noreply.github.com>
2021-09-27 17:34:07 -0400

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {  useDisclosure } from "@chakra-ui/react";
+import { useDisclosure } from "@chakra-ui/react";
 import { DAppProvider } from "@usedapp/core";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { Hydrate } from "react-query/hydration";
