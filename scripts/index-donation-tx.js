78695b2d6c1b7cc6ed2b8263750e071be2eb728b
Manuel Araoz <287189+maraoz@users.noreply.github.com>
2021-10-04 12:28:27 -0500

    fix indexing scripts for gnosis safe

--- scripts/index-donation-tx.js
+++ scripts/index-donation-tx.js
@@ -1,16 +1,12 @@
 const {ethers} = require("hardhat");
 
-const indexDonation = require("./lib/index-donation");
+const processTransaction = require("./lib/process-transaction");
 
 async function main() {
-  const txid = "0xc7495c4d762db9d1d8c1f2e4b3843624a8bfead3f88e415fe6ee6c0d4a26ea17";
-
-  const receipt = await ethers.provider.getTransactionReceipt(txid);
-  let abi = [ "event DonationReceived(address indexed donor, uint256 value, uint256 indexed tokenID)" ];
-  let iface = new ethers.utils.Interface(abi);
-  let log = iface.parseLog(receipt.logs[1]);
-  const {donor, value: amount, tokenID} = log.args;
-  await indexDonation(donor, amount, tokenID, txid);
+  const txid = "0xe2b62497141b753d73b38a13422093fa70fcae45139f673eba1cb4d992992634";
+  
+  const tx = await ethers.provider.getTransaction(txid);
+  await processTransaction(tx)
 }
 
 main()
