e17426b866efd5823b04e2a969861b47047ed44a
Manuel Araoz <287189+maraoz@users.noreply.github.com>
2021-10-09 11:30:07 -0500

    better errors for misconfig

--- scripts/lib/firestore.js
+++ scripts/lib/firestore.js
@@ -1,4 +1,17 @@
 var admin = require('firebase-admin');
+
+if (!process.env.FIREBASE_PROJECT_ID) {
+  throw new Error('Must set $FIREBASE_PROJECT_ID env variable');
+}
+
+if (!process.env.FIREBASE_CLIENT_EMAIL) {
+  throw new Error('Must set $FIREBASE_CLIENT_EMAIL env variable');
+}
+
+if (!process.env.FIREBASE_PRIVATE_KEY) {
+  throw new Error('Must set $FIREBASE_PRIVATE_KEY env variable');
+}
+
 const app = admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
