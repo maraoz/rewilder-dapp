68988d54309b51f117d8c30b35437cab0af330fe
Manuel Araoz <287189+maraoz@users.noreply.github.com>
2021-10-09 11:09:24 -0500

    use a StaticJsonProvider for live indexer

--- scripts/watch-donations.js
+++ scripts/watch-donations.js
@@ -3,28 +3,31 @@ const addresses = require("./lib/addresses");
 const indexDonation = require("./lib/index-donation");
 
 async function main() {
+
+  if (!process.env.INDEXER_INFURA_ID) {
+    throw new Error('Please set env variable $INDEXER_INFURA_ID with an infura app id');
+  }
+  const url = `https://${network.name}.infura.io/v3/${process.env.INDEXER_INFURA_ID}`;
+  console.log('Connecting to', url);
+  const provider = new ethers.providers.StaticJsonRpcProvider(url);
+
   // get donation campaign
   const RewilderDonationCampaign = await ethers.getContractFactory("RewilderDonationCampaign");
-
   const campaign = RewilderDonationCampaign
       .attach(addresses.RewilderDonationCampaign);
   console.log("RewilderDonationCampaign attached to:", campaign.address);
 
   // block tick
-  ethers.provider.on("block", (blockNumber) => {
+  provider.on("block", (blockNumber) => {
     if (blockNumber % 240 == 0) {
       console.log("block", blockNumber, "mined at", new Date().getTime());
     }
-  });
-  // Emitted when any new pending transaction is noticed
-  ethers.provider.on("pending", (tx) => {
-    console.log("pending tx", tx.hash);
   });
 
   // listen for events
   console.log("Registering Donation event handler")
 
-  campaign.on('DonationReceived', async function(donor, amount, tokenID, event) {
+  campaign.connect(provider).on('DonationReceived', async function(donor, amount, tokenID, event) {
     const txid = event.transactionHash;
     await indexDonation(donor, amount, tokenID, txid);
   });
