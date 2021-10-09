c0a671306a60d116817471a81b0b1741d7b0e171
Manuel Araoz <287189+maraoz@users.noreply.github.com>
2021-10-09 11:24:25 -0500

    use credentials from env

--- scripts/lib/firestore.js
+++ scripts/lib/firestore.js
@@ -1,13 +1,11 @@
 var admin = require('firebase-admin');
-var serviceAccount = require("../../rewilder-dev-firebase.json");
-if (process.env.REWILDER_ENV == "staging") {
-  serviceAccount = require("../../rewilder-staging-firebase.json")
-}
-if (process.env.REWILDER_ENV == "production") {
-  serviceAccount = require("../../rewilder-production-firebase.json")
-}
 const app = admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  }),
+  databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`,
 });
 const db = admin.firestore(app);
 
